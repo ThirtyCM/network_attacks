b60227daa278950ee4b2e4db8a8738a50f80afcc
ThirtyCM <sebvercammen@outlook.be>
2025-04-17 08:54:20 +0000

    init

---
+++ basic_setup.sh
@@ -0,0 +1,7 @@
+ws2 nft -f ./basic_firewalls/workstation.nft
+ws3 nft -f ./basic_firewalls/workstation.nft
+http nft -f ./basic_firewalls/dmz.nft
+ntp nft -f ./basic_firewalls/dmz.nft
+dns nft -f ./basic_firewalls/dmz.nft
+ftp nft -f ./basic_firewalls/dmz.nft
+internet nft -f ./basic_firewalls/internet.nft
---
+++ flush.sh
@@ -0,0 +1,7 @@
+ws2 nft flush ruleset
+ws3 nft flush ruleset
+http nft flush ruleset
+ntp nft flush ruleset
+dns nft flush ruleset
+ftp nft flush ruleset
+internet nft flush ruleset
